795dd36f05bce96972c239baa0a61b9bf8a0b78c
Turin1984 <turin84@mail.ru>
2022-06-19 19:33:24 +0300

    Задача №2 - Решение

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,23 +1,27 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 Console.Clear ();
 
-int prompt(string message)
+int Prompt(string message)
 {
-Console.WriteLine(message);
+Console.Write(message);
 string number = Console.ReadLine();
-int nuInt = int.Parse(number);
+int numInt = int.Parse(number);
 return numInt;
 }
+int x1 = Prompt("Введите координаты x1 = ");
+int y1 = Prompt("Введите координаты y1 = ");
+int z1 = Prompt("Введите координаты z1 = ");
+int x2 = Prompt("Введите координаты x2 = ");
+int y2 = Prompt("Введите координаты y2 = ");
+int z2 = Prompt("Введите координаты z2 = ");
 
-int x = Promt ("Введите 1й координат = ")
-int x = Promt ("Введите 1й координат = ")
-int x = Promt ("Введите 1й координат = ")
-int x = Promt ("Введите 1й координат = ")
-int x = Promt ("Введите 1й координат = ")
-int x = Promt ("Введите 1й координат = ")
+int a = x1-x2;
+int b = y1-y2;
+int c = z1-z2;
 
-//if ( x==0 || y==0 || z==0)
-{
-   // SystemConsole.WriteLine("Одна из координат равна нулю");
-}
+double distance = Math.Sqrt(a*a+b*b+c*c);
+
+Console.WriteLine ("Расстояние между 2-я точками в 3D пространстве равно =>" + distance);
+
+
 
