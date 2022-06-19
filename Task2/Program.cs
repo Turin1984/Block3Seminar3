13aae69e89e2777b0eb341e07ae5f7f1c7fe4177
Turin1984 <turin84@mail.ru>
2022-06-19 18:43:21 +0300

    Задача №2 - Создана папака в репозитории на GH

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear ();
+
+int prompt(string message)
+{
+Console.WriteLine(message);
+string number = Console.ReadLine();
+int nuInt = int.Parse(number);
+return numInt;
+}
+
+int x = Promt ("Введите 1й координат = ")
+int x = Promt ("Введите 1й координат = ")
+int x = Promt ("Введите 1й координат = ")
+int x = Promt ("Введите 1й координат = ")
+int x = Promt ("Введите 1й координат = ")
+int x = Promt ("Введите 1й координат = ")
+
+//if ( x==0 || y==0 || z==0)
+{
+   // SystemConsole.WriteLine("Одна из координат равна нулю");
+}
+
